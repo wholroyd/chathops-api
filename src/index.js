046fa69605e451f8c69f00b50b70454caf03f824
William Holroyd <wholroyd@gmail.com>
2015-07-04 22:55:59 -0400

    Getting started on api code, now in a development branch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 module.exports = (function() {
     
     var express = require('express');
+    var passport = require('passport');
     var app = express();
+
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.session({ secret: 'SECRET' }));
+    app.use(passport.initialize());
+    app.use(passport.session());
     
     app.get('/', function(req, res) {
         res.send('This is the api endpoint');  
     });
+
+    // This is used for the clients to report crashes
+    app.get('/crashreport', function(req, res) {
+
+    });
+
+    /* ### Slack Endpoints ### */
+
+    app.get('/auth/slack',
+        passport.authorize('slack'));
+
+    app.get('/auth/slack/callback',
+        passport.authorize('slack', { failureRedirect: '/login' }),
+        function(req, res) {
+            res.render();
+            // Successful authentication, redirect home.
+            //res.redirect('/');
+        });
+
+    /* ### Error handlers ### */
 
     // -- register 404 handler
     app.use(function(req, res, next) {
